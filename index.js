de5c7785f57b78dfd476594633641a051748936e
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2023-01-17 14:26:36 +0100

    Update yml

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Get route
 app.get('/', (req, res) => {
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at port:${port}`);
 });
