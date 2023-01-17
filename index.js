5fd116d040b921c12e0cc0d01f4d91ce9d524456
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2023-01-17 14:29:46 +0100

    Add commit to JS, build app

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// main page route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
